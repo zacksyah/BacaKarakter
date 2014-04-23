dbc1a6ba32178b60fa1a88a26db8defa02b08169
zacksyah <slash_jak@yahoo.com>
2014-04-23 20:17:55 +0700

    Bahasa C pertamax, first commit

---
+++ PembacaKarakter.c
@@ -0,0 +1,11 @@
+/*Pembaca Karakter*/
+#include <stdio.h>
+
+main() {
+    char kar;
+    printf("Program membaca karakter\n");
+    printf("Masukkan sebuah karakter : ");
+    scanf("%c", &kar);
+    printf("Karakter yang dimasukkan : %c\n", kar);
+    return 0;
+}
---
+++ build/Debug/Cygwin_4.x-Windows/PembacaKarakter.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_4.x-Windows/PembacaKarakter.o: PembacaKarakter.c
